7815e54e544ed2cbaefd03ff71ec51c7bbbf546b
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-07-11 16:04:23 +0200

    decorate

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,16 @@
 import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
+class Note {
+  constructor(public data: string[] = ["Moonhalo"]) {}
+}
+
 const app = new Elysia()
   .use(swagger())
+  .decorate("note", new Note())
+  .get("/note", ({ note }) => {
+    return note.data;
+  })
   .get("/", () => "Hello Elysia")
   .post("/hello", ({ body }) => {
     return body;
